f2a39dc8b7240e7e3627188f4e5f974895f22031
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-08-31 13:32:49 +0300

    Test Patch Lab Smali hook templates

---
+++ app/src/test/java/org/unirevlab/security/analysis/PatchLabEngineTest.kt
@@ -0,0 +1,46 @@
+package org.unirevlab.security.analysis
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class PatchLabEngineTest {
+    @Test
+    fun entryLogHookAddsTwoLocalsAndLogCall() {
+        val source = """
+            .class public Lx/Test;
+            .super Ljava/lang/Object;
+
+            .method public check(I)Z
+                .locals 1
+                const/4 v0, 0x1
+                return v0
+            .end method
+        """.trimIndent()
+
+        val patched = PatchLabEngine.addEntryLogHook(source, "check", "(I)Z", "TEST-001")
+
+        assertTrue(patched.contains(".locals 3"))
+        assertTrue(patched.contains("Landroid/util/Log;->d(Ljava/lang/String;Ljava/lang/String;)I"))
+        assertTrue(patched.contains("TEST-001: check(I)Z"))
+    }
+
+    @Test
+    fun forceBooleanAddsImmediateReturn() {
+        val source = """
+            .class public Lx/Test;
+            .super Ljava/lang/Object;
+
+            .method public enabled()Z
+                .locals 0
+                const/4 v0, 0x1
+                return v0
+            .end method
+        """.trimIndent()
+
+        val patched = PatchLabEngine.forceBooleanReturn(source, "enabled", "()Z", false)
+
+        assertTrue(patched.contains(".locals 1"))
+        assertTrue(patched.contains("const/16 v0, 0x0"))
+        assertTrue(patched.contains("return v0"))
+    }
+}
