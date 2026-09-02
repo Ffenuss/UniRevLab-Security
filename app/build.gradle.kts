e9577e17c38e29736510a3464a4a48546258e182
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-09-02 18:18:26 +0300

    Bump product preview to v0.29.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.unirevlab.security"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
-        versionName = "0.28.0-preview"
+        versionCode = 40
+        versionName = "0.29.0-preview"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
