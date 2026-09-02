cec6b3b0d744e50569e7b400d7cb17a755c419e5
Ffenuss <78445717+Ffenuss@users.noreply.github.com>
2026-09-02 17:36:37 +0300

    Bump product preview to v0.28.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.unirevlab.security"
         minSdk = 26
         targetSdk = 36
-        versionCode = 38
-        versionName = "0.25.9-dev-automod-navigation"
+        versionCode = 39
+        versionName = "0.28.0-preview"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
